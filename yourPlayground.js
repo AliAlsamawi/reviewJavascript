e7351483b032c36bbf0e2042661391f9da3855db
Ali A <alsamawiali4@gmail.com>
2022-09-29 20:00:19 -0400

    promt

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,7 +1,17 @@
-console.log('hello world')
+// console.log('hello world')
 
-youName = 'ali'
-console.log(youName)
+// youName = 'ali'
+// console.log(youName)
 
-food = 20
-console.log(food)
+//operators
+
+// fruit = prompt('what is your favorit fruit ?')
+
+// console.log(fruit)
+food = prompt('enter your food amount')
+// console.log(food)
+tipPercentage = prompt('tip precentage') /100
+tipamount = tipPercentage * food
+console.log('your time precent is', tipamount)
+
+//USER INPUT
