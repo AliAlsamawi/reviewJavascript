5432cd8fbb69ced74fff41ce7df761567887b46b
Ali A <alsamawiali4@gmail.com>
2022-10-02 12:37:37 -0400

    waether app

--- yourPlayground.js
+++ yourPlayground.js
@@ -8,16 +8,29 @@
 // fruit = prompt('what is your favorit fruit ?')
 
 // console.log(fruit)
-food = Number(prompt('enter your food amount'))
+// food = Number(prompt('enter your food amount'))
 // console.log(food)
-tipPercentage = Number(prompt('tip precentage') /100)
-tipamount = tipPercentage * food
-console.log('your tip precent is', tipamount)
-total = food + tipamount
-console.log(total, 'is your total')
+// tipPercentage = Number(prompt('tip precentage') /100)
+// tipamount = tipPercentage * food
+// console.log('your tip precent is', tipamount)
+// total = food + tipamount
+// console.log(total, 'is your total')
 
 
 //USER INPUT
 //user input
 
-// datat 
+//math methods
+// Math.floor(rounds down)
+// Math.ceil(round up)
+// Math.random(random number)
+
+// variable(var, let, const)
+
+let weather = prompt("what's the weather")
+if (weather == 'rain'){
+  console.log('get your umbrella ')
+} else {
+  console.log('get you sun glasses')
+}
+
